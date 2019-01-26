52146394c7f112c5fa4e240fc714335c3bbee807
Matthew Philps <philps.matthew@gmail.com>
2019-01-26 20:14:40 +0900

    tests passing for findGear function

--- src/equipmentList.spec.js
+++ src/equipmentList.spec.js
@@ -1,65 +1,16 @@
-const equipment = require('./equipmentList')
-
-const list = {
-    basicPouch:{
-        name: "Basic Pouch",
-        weight: 0.4,
-        type: ['loadBearing', 'alice']
-    },
-    belt:{
-        name: "Belt",
-        weight: 0.7,
-        type: ['loadBearing', 'alice']
-    },
-    beltPistolM1923:{
-        name: "Belt, pistol M1923",
-        weight: 0.56,
-        type: ['loadBearing', 'ww2']
-    },
-    bumPack:{
-        name: "Bum Pack",
-        weight: 0.7,
-        type: ['loadBearing', 'alice']
-    },
-    canteen:{
-        name: "Canteen",
-        weight: 2.5,
-        type: ['loadBearing', 'alice']
-    },
-    hHarness:{
-        name: "H Harness",
-        weight: 0.7,
-        type: ['loadBearing', 'alice']
-    }
-}
-
-const findGear = function(list, findName){
-    for(let obj in list){
-        if(list[obj].name === findName){
-            return list[obj]
-        }
-    }
-}
-
-const findType = function(list, findType){
-    let results = []
-    for(let obj in list){
-        if(list[obj].type.includes(findType)){
-            results.push(list[obj])
-        }
-    }
-    return results
-}
+const {findGear, filterEquipment} = require('./equipmentListFunctions')
 
 describe("finding things in the equipment list",()=>{
     it('should return items based on name',()=>{
-        expect(findGear(list, 'Belt').weight).toBe(0.7)
-        expect(findGear(list, "Canteen").weight).toBe(2.5)
+        expect(findGear('Belt').weight).toBe(0.7)
+        expect(findGear("Canteen").weight).toBe(2.5)
+        expect(findGear("Rock").weight).toBe(1.5)
+        expect(findGear("Combat Stretcher, Folding").weight).toBe(15)
     })
     it('should filter based on type key value',()=>{
-        expect(findType(list,"ww2").length).toBe(1)
-        expect(findType(list, 'ww2')[0].weight).toBe(0.56)
-        expect(findType(list, 'alice').length).toBe(5)
-        expect(findType(list, 'loadBearing').length).toBe(6)
+        expect(filterEquipment("WW2").length).toBe(14)
+        expect(filterEquipment('WW2')[0].weight).toBe(1.56)
+        expect(filterEquipment('ALICE').length).toBe(5)
+        expect(filterEquipment('Load Bearing').length).toBe(34)
     })
 })
---
+++ src/equipmentListFunctions.js
@@ -0,0 +1,22 @@
+const equipment = require('./equipmentList')
+
+const findGear = function(findName){
+    for(let obj in equipment){
+        if(obj === findName){
+            return equipment[obj]
+        }
+    }
+}
+
+
+const filterEquipment = function(findType){
+    let results = []
+    for(let obj in equipment){
+        if(equipment[obj].tags.includes(findType)){
+            results.push(equipment[obj])
+        }
+    }
+    return results
+}
+
+module.exports = {findGear, filterEquipment}
