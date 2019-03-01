b6008f80f5bbf43e3ea778851697f1d4c76d8ea4
Matthew Philps <philps.matthew@gmail.com>
2019-03-01 12:48:05 +0900

    test passed

--- src/equipmentList.spec.js
+++ src/equipmentList.spec.js
@@ -11,7 +11,7 @@ describe("finding things in the equipment list",()=>{
     })
 })
     
-describe.only('filtering the equipment list',()=>{
+describe('filtering the equipment list',()=>{
     it('should filter based on type key value',()=>{
         expect(Object.keys(filterEquipment(["WW2"])).length).toBe(14)
         expect(filterEquipment(['WW2'])["Bayonet, M1 w/M7 scabard"].weight).toBe(1.56)
@@ -23,12 +23,11 @@ describe.only('filtering the equipment list',()=>{
         expect(filterEquipment(['WW2'])).toHaveProperty('Bayonet, M1 w/M7 scabard')
     })
     it('should be able to filter more than one tag',()=>{
-        // rations 12, medical 9
         const filteredList = filterEquipment(["rations","medical"])
         expect(Object.keys(filteredList).length).toBe(21)
     })
     it('should not filter if parameter is empty array',()=>{
-        expect(filterEquipment().length).toBe(88)
+        expect(Object.keys(filterEquipment([])).length).toBe(88)
     })
 })    
 
