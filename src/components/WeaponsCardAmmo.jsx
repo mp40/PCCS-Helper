de213dd63366f0c57dc3cefeb7f391d87616f7cc
Matthew Philps <philps.matthew@gmail.com>
2019-05-06 21:44:20 +0900

    refactoring weapons card

---
+++ src/components/WeaponsCardAmmo.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+
+export class WeaponsCardAmmo extends Component {
+
+    render() {
+        return (
+            <div>
+                <div className="additionalAmmoTag">
+                    additional ammo
+                </div>
+
+                {this.props.gunObj.mag.map((magObj, index)=>{
+                    return <div key={index} className="spareMags">
+                                <span>{magObj.qty} x </span>
+                                {magObj.type !== 'Rnd' ?
+                                <span>{magObj.cap} round {magObj.type}</span> :
+                                <span>single rounds</span>
+                                }
+                                <span>(??) lbs</span>
+                                {/* <span className="arrowBox" style={{width:'9%'}}> */}
+                                <button 
+                                id="qtyUpMag"
+                                className="equipmentButton"
+                                // onClick={this.handleIncrementGunQty.bind(this,gunObj,1)}
+                                >
+                                  {String.fromCharCode(8593)}
+                                </button>
+                                <button 
+                                id="qtyDownMag" 
+                                className="equipmentButton"
+                                // onClick={this.handleIncrementGunQty.bind(this,gunObj,-1)}
+                                >
+                                  {String.fromCharCode(8595)}
+                                </button>
+                              {/* </span> */}
+                              </div>
+                 })}
+            </div>
+        )   
+    }
+
+}
+
+export default WeaponsCardAmmo
