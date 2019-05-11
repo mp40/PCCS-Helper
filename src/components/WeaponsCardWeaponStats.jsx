6a0ba85a51cd3e6aba2a356840fc9c15e791bd8d
Matthew Philps <philps.matthew@gmail.com>
2019-05-11 22:26:31 +0900

    wip weapon stat table

--- src/components/WeaponsCardWeaponStats.jsx
+++ src/components/WeaponsCardWeaponStats.jsx
@@ -63,6 +63,140 @@ export class WeaponsCardWeaponStats extends Component {
                                 return <td key={index}>{dcData}</td>
                             })}
                         </tr>
+                        {/* third row */}
+                        <tr id="GunTableLineThree">
+                            <td></td>
+                            <td>
+                                <span>{gunObj.aim.ac[2]}</span>
+                                <span>{gunObj.aim.mod[2]}</span>
+                            </td>
+                        </tr>
+                        {/* fourth row */}
+                        <tr id="GunTableLineFour">
+                            <td id="WeaponStatReload">
+                                <span>RT</span>
+                                <span>{gunObj.rt}</span>
+                            </td>
+                            <td>
+                                <span>{gunObj.aim.ac[3]}</span>
+                                <span>{gunObj.aim.mod[3]}</span>
+                            </td>
+                            <td>
+                                <span>{gunObj.projectiles[1].type}</span>
+                                <span>PEN</span>
+                            </td>
+                            {gunObj.projectiles[1].pen.map((penData, index)=>{
+                                return penData.toString().length === 1 ?
+                                <td key={index}>{penData.toFixed(1)}</td> :
+                                <td key={index}>{penData}</td>
+                            })}
+                        </tr>
+                        {/* fifth row */}
+                        <tr id="GunTableLineFive">
+                            <td id="WeaponStatROF">
+                                <span>ROF</span>
+                                <span>{gunObj.rof}</span>
+                            </td>
+                            <td>
+                                <span>{gunObj.aim.ac[4]}</span>
+                                <span>{gunObj.aim.mod[4]}</span>
+                            </td>
+                            <td>
+                                <span>DC</span>
+                            </td>
+                            {gunObj.projectiles[1].dc.map((dcData, index)=>{
+                                return <td key={index}>{dcData}</td>
+                            })}
+                        </tr>
+                        {/* sixth row */}
+                        <tr id="GunTableLineSix">
+                            <td></td>
+                            <td>
+                                <span>{gunObj.aim.ac[5]}</span>
+                                <span>{gunObj.aim.mod[5]}</span>
+                            </td>
+                        </tr>
+                        {/* seventh row */}
+                        <tr id="GunTableLineSeven">
+                            <td id="WeaponStatCapacity">
+                                <span>Cap</span>
+                                <span>{gunObj.mag[0].cap}</span>
+                            </td>
+                            <td>
+                                <span>{gunObj.aim.ac[6]}</span>
+                                <span>{gunObj.aim.mod[6]}</span>
+                            </td>
+                            <td>
+                                <span>{gunObj.projectiles[2].type}</span>
+                                <span>PEN</span>
+                            </td>
+                            {gunObj.projectiles[2].pen.map((penData, index)=>{
+                                return penData.toString().length === 1 ?
+                                <td key={index}>{penData.toFixed(1)}</td> :
+                                <td key={index}>{penData}</td>
+                            })}
+                        </tr>
+                        {/* eighth row */}
+                        <tr id="GunTableLineEight">
+                            <td id="WeaponStatAW">
+                                <span>AW</span>
+                                <span>{gunObj.mag[0].weight}</span>
+                            </td>
+                            <td>
+                                <span>{gunObj.aim.ac[7]}</span>
+                                <span>{gunObj.aim.mod[7]}</span>
+                            </td>
+                            <td>
+                                <span>DC</span>
+                            </td>
+                            {gunObj.projectiles[2].dc.map((dcData, index)=>{
+                                return <td key={index}>{dcData}</td>
+                            })}
+                        </tr>
+                        {/* ninth row */}
+                        <tr id="GunTableLineNine">
+                            <td>
+                                <span>{gunObj.mag[0].type}</span>
+                            </td>
+                            <td>
+                                <span>{gunObj.aim.ac[8]}</span>
+                                <span>{gunObj.aim.mod[8]}</span>
+                            </td>
+                        </tr>
+                        {/* tenth row */}
+                        <tr id="GunTableLineTen">
+                            <td id="WeaponStatKnockDown">
+                                <span>KD</span>
+                                <span>{gunObj.kd}</span>
+                            </td>
+                            <td>
+                                <span>{gunObj.aim.ac[9]}</span>
+                                <span>{gunObj.aim.mod[9]}</span>
+                            </td>
+                            <td>
+                                <span>BA</span>
+                            </td>
+                            {gunObj.ba.map((dcData, index)=>{
+                                return <td key={index}>{dcData}</td>
+                            })}
+                        </tr>
+                        {/* eleventh row */}
+                        <tr id="GunTableLineEleven">
+                            <td id="WeaponStatSAB">
+                                <span>SAB</span>
+                                <span>{gunObj.sab}</span>
+                            </td>
+                            <td>
+                                <span>{gunObj.aim.ac[10]}</span>
+                                <span>{gunObj.aim.mod[10]}</span>
+                            </td>
+                            <td>
+                                <span>TOF</span>
+                            </td>
+                            {gunObj.tof.map((dcData, index)=>{
+                                return <td key={index}>{dcData}</td>
+                            })}
+                        </tr>
                     </tbody>
 
                 </table>
