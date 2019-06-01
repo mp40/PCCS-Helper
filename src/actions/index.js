738077df70f0326911c4a0d64b972bc185da31fe
Matthew Philps <philps.matthew@gmail.com>
2019-06-01 11:09:32 +0900

    linted actions and reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,61 +1,48 @@
-const { calculateStateObject } = require("../helpers/helperFunctions");
+const { calculateStateObject } = require('../helpers/helperFunctions');
 
-export const selectCurrentView = (view) => {
-    return {
-        type: 'VIEW_SELECTED',
-        payload: view
-    }
-}
+export const selectCurrentView = view => ({
+  type: 'VIEW_SELECTED',
+  payload: view,
+});
 
-export const updateWeight = (newWeight, attributeObj) => {
-    return (dispatch) => {
-        dispatch({
-            type: 'TOTAL_WEIGHT',
-            payload: newWeight
-        })
-        dispatch(updateCombatStats(attributeObj, newWeight))
-    }
-}
+export const updateCombatStats = (attributeObj, weight = 0) => {
+  const newCombatData = calculateStateObject(attributeObj, weight);
+  return {
+    type: 'UPDATE_ALL_COMBAT_STATS',
+    payload: newCombatData,
+  };
+};
 
-export const updateAttributes = (attributeObj, weight) => {
-    return (dispatch) => {
-        dispatch({
-            type: 'UPDATE_ATTRIBUTES',
-            payload: attributeObj
-        })
-        dispatch(updateCombatStats(attributeObj, weight))
-    }
-}
+export const updateWeight = (newWeight, attributeObj) => (dispatch) => {
+  dispatch({
+    type: 'TOTAL_WEIGHT',
+    payload: newWeight,
+  });
+  dispatch(updateCombatStats(attributeObj, newWeight));
+};
 
-export const updateCombatStats = (attributeObj, weight=0) => {
-    const newCombatData = calculateStateObject(attributeObj, weight)
-    return {
-        type: 'UPDATE_ALL_COMBAT_STATS',
-        payload: newCombatData
-    }
-}
+export const updateAttributes = (attributeObj, weight) => (dispatch) => {
+  dispatch({
+    type: 'UPDATE_ATTRIBUTES',
+    payload: attributeObj,
+  });
+  dispatch(updateCombatStats(attributeObj, weight));
+};
 
-export const modifyEquipment = (newWeight, equipArray, attributeObj) => {
-    return (dispatch) => {
-        dispatch({type:'MODIFY_EQUIPMENT', payload: equipArray})
-        dispatch(updateWeight(newWeight, attributeObj))
-    }
-}
+export const modifyEquipment = (newWeight, equipArray, attributeObj) => (dispatch) => {
+  dispatch({ type: 'MODIFY_EQUIPMENT', payload: equipArray });
+  dispatch(updateWeight(newWeight, attributeObj));
+};
 
-export const changeUniform = (newUniform, newWeight, attributeObj) => {
-    return (dispatch) => {
-        dispatch({
-            type: 'CHANGE_UNIFORM',
-            payload: newUniform
-        })
-        dispatch(updateWeight(newWeight, attributeObj))
-    }
-}
+export const changeUniform = (newUniform, newWeight, attributeObj) => (dispatch) => {
+  dispatch({
+    type: 'CHANGE_UNIFORM',
+    payload: newUniform,
+  });
+  dispatch(updateWeight(newWeight, attributeObj));
+};
 
-export const modifyFirearmList = (newWeight, firearmsArray, attributeObj) => {
-    return (dispatch) => {
-        dispatch({type:'MODIFY_FIREARMS', payload: firearmsArray})
-        dispatch(updateWeight(newWeight, attributeObj))
-    }
-}
-
+export const modifyFirearmList = (newWeight, firearmsArray, attributeObj) => (dispatch) => {
+  dispatch({ type: 'MODIFY_FIREARMS', payload: firearmsArray });
+  dispatch(updateWeight(newWeight, attributeObj));
+};
--- src/reducers/gearReducer.js
+++ src/reducers/gearReducer.js
@@ -1,22 +1,20 @@
-// import { combineReducers } from 'redux';
-
 export const modifyEquipmentReducer = (equipArray = [], action) => {
-    if (action.type === 'MODIFY_EQUIPMENT'){
-        return action.payload
-    }
-    return equipArray
-}
+  if (action.type === 'MODIFY_EQUIPMENT') {
+    return action.payload;
+  }
+  return equipArray;
+};
 
 export const changeUniformReducer = (uniform = 'Normal', action) => {
-    if (action.type === 'CHANGE_UNIFORM'){
-        return action.payload
-    }
-    return uniform
-}
+  if (action.type === 'CHANGE_UNIFORM') {
+    return action.payload;
+  }
+  return uniform;
+};
 
 export const firearmsReducer = (firearmArray = [], action) => {
-    if (action.type === 'MODIFY_FIREARMS'){
-        return action.payload
-    }
-    return firearmArray
-}
+  if (action.type === 'MODIFY_FIREARMS') {
+    return action.payload;
+  }
+  return firearmArray;
+};
