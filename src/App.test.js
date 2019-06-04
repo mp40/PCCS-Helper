937bd41f963ae1c233aa717a762a07f9e63c7b5f
Matthew Philps <philps.matthew@gmail.com>
2019-06-04 21:01:57 +0900

    removed redundant test

--- src/App.test.js
+++
@@ -1,6 +0,0 @@
-import { mountAppWithStore } from './helpers/testHelpers';
-
-it('renders without crashing', () => {
-  const wrapper = mountAppWithStore();
-  expect(wrapper.exists()).toBe(true);
-});
