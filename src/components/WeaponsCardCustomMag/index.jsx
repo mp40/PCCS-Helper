ab3a7804591ef990ef027eacb65e72c6cc7792d3
Matthew Philps <philps.matthew@gmail.com>
2019-06-04 22:12:13 +0900

    moved WeaponsCardCustomMag to container folder

--- src/components/WeaponsCardCustomMag/index.jsx
+++ src/components/WeaponsCardCustomMag/index.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import ButtonStandard from '../helpers/buttons/ButtonStandard';
-import './WeaponsCard/WeaponsCard.css';
+import ButtonStandard from '../../helpers/buttons/ButtonStandard';
+import '../WeaponsCard/WeaponsCard.css';
 
 class WeaponsCardCustomMag extends Component {
   constructor(props) {
