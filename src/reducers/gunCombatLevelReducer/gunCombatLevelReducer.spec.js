fdd883b97bda5201cf33abe6ba5374c6bfcc6e05
Matthew Philps <philps.matthew@gmail.com>
2019-06-11 09:40:38 +0900

    housekeeping

--- src/reducers/gunCombatLevelReducer/gunCombatLevelReducer.spec.js
+++ src/reducers/gunCombatLevelReducer/gunCombatLevelReducer.spec.js
@@ -1,28 +1,5 @@
 import { modifyGunCombatLevelReducer } from './index';
-
-const mockState = () => ({
-  totalWeight: 5,
-  characterStats: {
-    str: 10,
-    int: 10,
-    hlt: 10,
-    wil: 10,
-    agi: 10,
-    gunLevel: 0,
-    handLevel: 0,
-  },
-  combatStats: {
-    baseSpeed: 3,
-    maxSpeed: 6,
-    SAL: 0,
-    CE: 0,
-    ISF: 10,
-    ASF: 10,
-    knockoutValue: 5,
-    damageBonus: 1,
-    combatActions: [4, 4],
-  },
-});
+import { mockState } from '../mockState';
 
 describe('gunCombatLevelReducer function', () => {
   it('should return correct values when gunLevel changes to 1', () => {
--- src/reducers/strengthStatReducer/strengthStatReducer.spec.js
+++ src/reducers/strengthStatReducer/strengthStatReducer.spec.js
@@ -1,30 +1,7 @@
 import { modifyStrengthValueReducer } from './index';
+import { mockState } from '../mockState';
 
-const mockState = () => ({
-  totalWeight: 5,
-  characterStats: {
-    str: 10,
-    int: 10,
-    hlt: 10,
-    wil: 10,
-    agi: 10,
-    gunLevel: 0,
-    handLevel: 0,
-  },
-  combatStats: {
-    baseSpeed: 3,
-    maxSpeed: 6,
-    SAL: 0,
-    CE: 0,
-    ISF: 10,
-    ASF: 10,
-    knockoutValue: 5,
-    damageBonus: 1,
-    combatActions: [4, 4],
-  },
-});
-
-describe('gunCombatLevelReducer function', () => {
+describe('strengthStatReducer function', () => {
   it('should return correct values when strength changes to 3', () => {
     const action = { payload: 3 };
     const newState = modifyStrengthValueReducer(mockState(), action);
