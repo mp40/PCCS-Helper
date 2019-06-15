15a3592ce8d21a642ebe276c44a0310b52afc7cd
Matthew Philps <philps.matthew@gmail.com>
2019-06-15 16:14:00 +0900

    refactored AttributeCard to map StatInput

--- src/components/AttributeCard/component.jsx
+++ src/components/AttributeCard/component.jsx
@@ -3,13 +3,12 @@ import PropTypes from 'prop-types';
 import StatInput from '../widgets/StatInput';
 import { isValidAttributeStat } from '../../helpers/gaurds';
 
+const attributeArray = ['str', 'int', 'hlt', 'wil', 'agi'];
+const statNameArray = ['Strength', 'Intelligence', 'Health', 'Willpower', 'Agility'];
+
 const AttributeCard = (props) => {
-  const { characterStats,
-    modifyStrengthValue,
-    modifyIntelligenceValue,
-    modifyHealthValue,
-    modifyWillpowerValue,
-    modifyAgilityValue } = props;
+  const { characterStats } = props;
+  const actionsArray = Object.values(props).slice(-5);
 
   return (
     <div>
@@ -20,41 +19,17 @@ const AttributeCard = (props) => {
               <th className="attHeading">Attribute</th>
               <th className="attValHeading">Value</th>
             </tr>
-            <StatInput
-              statLevel={characterStats.str}
-              statName="Strength"
-              idRef="updateStr"
-              isValid={isValidAttributeStat}
-              action={modifyStrengthValue}
-            />
-            <StatInput
-              statLevel={characterStats.int}
-              statName="Intelligence"
-              idRef="updateInt"
-              isValid={isValidAttributeStat}
-              action={modifyIntelligenceValue}
-            />
-            <StatInput
-              statLevel={characterStats.hlt}
-              statName="Health"
-              idRef="updateHlt"
-              isValid={isValidAttributeStat}
-              action={modifyHealthValue}
-            />
-            <StatInput
-              statLevel={characterStats.wil}
-              statName="Willpower"
-              idRef="updateWil"
-              isValid={isValidAttributeStat}
-              action={modifyWillpowerValue}
-            />
-            <StatInput
-              statLevel={characterStats.agi}
-              statName="Agility"
-              idRef="updateAgi"
-              isValid={isValidAttributeStat}
-              action={modifyAgilityValue}
-            />
+            {attributeArray.map((stat, index) => (
+              <StatInput
+                statLevel={characterStats[stat]}
+                statName={statNameArray[index]}
+                idRef={`update${stat.charAt(0).toUpperCase() + stat.slice(1)}`}
+                isValid={isValidAttributeStat}
+                action={actionsArray[index]}
+                key={stat}
+              />
+            ))
+            }
           </tbody>
         </table>
       </div>
@@ -63,11 +38,6 @@ const AttributeCard = (props) => {
 };
 
 AttributeCard.propTypes = {
-  modifyAgilityValue: PropTypes.func,
-  modifyWillpowerValue: PropTypes.func,
-  modifyHealthValue: PropTypes.func,
-  modifyIntelligenceValue: PropTypes.func,
-  modifyStrengthValue: PropTypes.func,
   characterStats: PropTypes.objectOf(PropTypes.number),
 };
 
