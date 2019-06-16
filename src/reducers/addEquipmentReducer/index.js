ca9034bbe025169d607f69645bdc8b3bd15b9cf6
Matthew Philps <philps.matthew@gmail.com>
2019-06-16 22:01:02 +0900

    added additional test to addEquipment reducer

--- src/reducers/addEquipmentReducer/index.js
+++ src/reducers/addEquipmentReducer/index.js
@@ -3,7 +3,7 @@ export const addEquipmentReducer = (state, action) => {
   const newWeight = state.totalWeight + action.payload.weight;
 
   return { ...state,
-    totalWeight: newWeight,
+    totalWeight: Math.floor(newWeight * 1000) / 1000,
     gear: { ...state.gear,
       equipment: [...state.gear.equipment, action.payload] } };
 };
--- src/reducers/addEquipmentReducer/testResources.js
+++ src/reducers/addEquipmentReducer/testResources.js
@@ -1,17 +1,34 @@
 import { MockState } from '../mockState';
 
-export const testEquipment = () => ({
-  name: 'testEquipment',
-  weight: 1.53,
-  tags: ['test'],
-  qty: 1,
-});
+export class Equipment {
+  constructor() {
+    this.name = 'testEquipment';
+    this.weight = 1.53;
+    this.tags = ['test'];
+    this.qty = 1;
+  }
+}
 
+export class OtherEquipment extends Equipment {
+  constructor() {
+    super();
+    this.name = 'otherEquipment';
+    this.weight = 2.47;
+  }
+}
 
 export class AddedEquipment extends MockState {
   constructor() {
     super();
     this.totalWeight = 6.53;
-    this.gear.equipment = [testEquipment()];
+    this.gear.equipment = [new Equipment()];
+  }
+}
+
+export class AddedEquipmentAgain extends AddedEquipment {
+  constructor() {
+    super();
+    this.totalWeight = 9;
+    this.gear.equipment = [new Equipment(), new OtherEquipment()];
   }
 }
