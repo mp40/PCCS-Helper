6a00b0408ee1139f41849968e480888bfdf999d0
Matthew Philps <philps.matthew@gmail.com>
2019-06-27 08:04:46 +0900

    fixed and passed  tests

--- src/reducers/modifyFirearmReducer/index.js
+++ src/reducers/modifyFirearmReducer/index.js
@@ -1,10 +1,11 @@
 export const modifyFirearmReducer = (state, action) => {
-  const newWeight = state.totalWeight + action.payload.modification.weight.weightMod;
+  const newWeight = state.totalWeight + action.payload.modNote.weightMod;
 
   const updatedFirearmsArray = state.gear.firearms.map((element) => {
     if (element.name === action.payload.firearm) {
-      const element = modifiedFirearm;
-      modifiedFirearm.modNotes = action.payload.modification;
+      const modifiedFirearm = element;
+      modifiedFirearm.weight += action.payload.modNote.weightMod;
+      modifiedFirearm.modNote = action.payload.modNote;
       return modifiedFirearm;
     }
     return element;
@@ -14,21 +15,5 @@ export const modifyFirearmReducer = (state, action) => {
   return { ...state,
     totalWeight: Math.round(newWeight * 1000) / 1000,
     gear: { ...state.gear,
-      firearms: [...firearmArray] } };
+      firearms: updatedFirearmsArray } };
 };
-
-// import { modifyObjectQtyInArray } from '../../helpers/actionHelpers';
-
-// export const increaseMagazineReducer = (state, action) => {
-//   const newGunObj = action.payload.firearm;
-
-//   newGunObj.mag = modifyObjectQtyInArray(newGunObj.mag, action.payload.magazine, 1);
-//   const newGunArray = modifyObjectQtyInArray(state.gear.firearms, newGunObj);
-
-//   const newWeight = state.totalWeight + action.payload.magazine.weight;
-
-//   return { ...state,
-//     totalWeight: Math.round(newWeight * 1000) / 1000,
-//     gear: { ...state.gear,
-//       firearms: [...newGunArray] } };
-// };
