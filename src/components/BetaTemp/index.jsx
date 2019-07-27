e671a4046529949418045f92d8b9b9067697fbe8
Matthew Philps <philps.matthew@gmail.com>
2019-07-27 18:10:03 +0900

    add beta banner

---
+++ src/components/BetaTemp/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const BetaTemp = () => (
+  <div style={{ marginTop: '10rem' }}>
+    <div style={{ fontSize: '8rem', fontWeight: 'bold', opacity: '0.5' }}>
+    BETA VERSION
+    </div>
+    <div style={{ fontSize: '4rem', opacity: '0.5' }}>
+    In Development
+    </div>
+  </div>
+);
+
+export default BetaTemp;
+
+// todo delete this component once at MVP
