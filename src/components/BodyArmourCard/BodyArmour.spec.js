57a4577e08ad7c548db5c924d9ca330671719515
Matthew Philps <philps.matthew@gmail.com>
2019-07-29 09:17:12 +0900

    body armour wip

---
+++ src/components/BodyArmourCard/BodyArmour.spec.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import BodyArmourCard from './index';
+
+describe('the BodyArmourCard', () => {
+  // eslint-disable-next-line react/jsx-filename-extension
+  const wrapper = shallow(<BodyArmourCard />);
+  it('should...', () => {
+    // todo
+  });
+});
