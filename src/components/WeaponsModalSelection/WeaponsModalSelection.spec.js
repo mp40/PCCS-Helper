9600861f9b940db3db262d3188fa518df4c1ee3a
Matthew Philps <philps.matthew@gmail.com>
2019-08-15 18:22:48 +0900

    updated package and corrected async hooks tests

--- src/components/WeaponsModalSelection/WeaponsModalSelection.spec.js
+++ src/components/WeaponsModalSelection/WeaponsModalSelection.spec.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import { act } from 'react-dom/test-utils';
 import WeaponsModalSelection, { filterCalibersFromType } from './index';
-
 import { mgs } from '../../data/firearms';
 
-const waitOneTick = () => new Promise(((resolve) => {
+const waitOneTick = simulate => new Promise(((resolve) => {
   setTimeout(() => {
-    resolve();
+    resolve(simulate);
   }, 0);
+}));
+
+const waitOneSec = simulate => new Promise(((resolve) => {
+  setTimeout(() => {
+    resolve(simulate);
+  }, 1001);
 }));
 
 describe('promise to wait for transition close', () => {
@@ -18,14 +24,26 @@ describe('promise to wait for transition close', () => {
       expect(wrapper.find('.filterCardWrapper').exists()).toEqual(true);
       expect(wrapper.find('.filterCardWrapper').hasClass('trans')).toEqual(false);
     });
-    it('should be apply transition to filter card', async () => {
-      wrapper.find('#showFirearmFilters').simulate('click');
-      await waitOneTick();
+    it('should apply transition to filter card', async () => {
+      await act(async () => {
+        await waitOneTick(wrapper.find('#showFirearmFilters').simulate('click'));
+      });
+      wrapper.update();
       expect(wrapper.find('.filterCardWrapper').hasClass('trans')).toEqual(true);
+      expect(wrapper.find('.filterCardWrapper').hasClass('final')).toEqual(false);
+    });
+    it('should apply the final class after one second', async () => {
+      await act(async () => {
+        await waitOneSec();
+      });
+      wrapper.update();
+      expect(wrapper.find('.filterCardWrapper').hasClass('final')).toEqual(true);
     });
     it('should be possible to reverse filter card transition', async () => {
-      wrapper.find('#showFirearmFilters').simulate('click');
-      await waitOneTick();
+      await act(async () => {
+        await waitOneTick(wrapper.find('#showFirearmFilters').simulate('click'));
+      });
+      wrapper.update();
       expect(wrapper.find('.filterCardWrapper').hasClass('trans')).toEqual(false);
     });
   });
@@ -34,32 +52,22 @@ describe('promise to wait for transition close', () => {
     it('should not be rendered by default', () => {
       expect(wrapper.find('.WeaponStatTableContainer').exists()).toEqual(false);
     });
-    it('should render the card in pre-transition location then apply transition', (done) => {
-      // this test throws warning that I should be using act(...)
-      // have tried multiple ways to use this not no avail
-      // however there seems to be a lot of people only line complaining it is broken
-      // the below works, but still throws warnings
-      // ignoring these warnings for now
-      wrapper.find('#viewM60').simulate('click');
-      expect(wrapper.find('.WeaponStatTableContainer').exists()).toEqual(true);
-      expect(wrapper.find('.WeaponStatTableContainer').hasClass('trans')).toEqual(false);
-      setTimeout(() => {
-        wrapper.update();
-        expect(wrapper.find('.WeaponStatTableContainer').hasClass('trans')).toEqual(true);
-        done();
-      }, 0);
-    });
-    it('should reverse transtion before closing card', async (done) => {
+    it('should render the card in pre-transition location then apply transition', async () => {
+      expect(wrapper.find('.WeaponStatTableContainer').exists()).toEqual(false);
+      await act(async () => {
+        await waitOneTick(wrapper.find('#viewM60').simulate('click'));
+      });
+      wrapper.update();
       expect(wrapper.find('.WeaponStatTableContainer').hasClass('trans')).toEqual(true);
-      wrapper.find('#closeGunStatView').simulate('click');
-      await waitOneTick();
+    });
+    it('should reverse transtion before closing card', async () => {
+      expect(wrapper.find('.WeaponStatTableContainer').hasClass('trans')).toEqual(true);
+      await act(async () => {
+        await waitOneTick(wrapper.find('#closeGunStatView').simulate('click'));
+      });
+      wrapper.update();
       expect(wrapper.find('.WeaponStatTableContainer').hasClass('trans')).toEqual(false);
       expect(wrapper.find('.WeaponStatTableContainer').exists()).toEqual(true);
-      setTimeout(() => {
-        wrapper.update();
-        expect(wrapper.find('#closeGunStatView').exists()).toEqual(false);
-        done();
-      }, 1000);
     });
   });
 });
