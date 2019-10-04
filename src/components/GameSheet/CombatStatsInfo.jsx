1babdd9e9bceff807b910ca89ba3134af64c90c2
Matthew Philps <philps.matthew@gmail.com>
2019-10-04 16:53:37 +0900

    minor corrections

--- src/components/GameSheet/CombatStatsInfo.jsx
+++ src/components/GameSheet/CombatStatsInfo.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
+import { combatStatsShape } from '../../helpers/proptypeShapes';
 
 const renderStatLine = (name, value) => (
   <div style={{ display: 'flex', justifyContent: 'space-between' }}>
@@ -10,12 +11,12 @@ const renderStatLine = (name, value) => (
 
 const CombatStatsInfo = ({ combatStats, gunLevel, handLevel }) => (
   <div className="combatStatsInfo">
-    <div className="comabatStatColA">
+    <div className="combatStatColA">
       {renderStatLine('Base Speed', combatStats.baseSpeed)}
       {renderStatLine('Max Speed', combatStats.maxSpeed)}
       {renderStatLine('Knockout Val', combatStats.knockoutValue)}
     </div>
-    <div className="comabatStatColB">
+    <div className="combatStatColB">
       {renderStatLine('Gun Combat', gunLevel)}
       {renderStatLine('Melee Combat', handLevel)}
       {renderStatLine('Damage Bonus', combatStats.damageBonus)}
@@ -24,8 +25,7 @@ const CombatStatsInfo = ({ combatStats, gunLevel, handLevel }) => (
 );
 
 CombatStatsInfo.propTypes = {
-  // eslint-disable-next-line react/forbid-prop-types
-  combatStats: PropTypes.object,
+  combatStats: combatStatsShape,
   gunLevel: PropTypes.number,
   handLevel: PropTypes.number,
 };
