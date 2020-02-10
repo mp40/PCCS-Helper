9fd275bc2c8d8095267df4767041c0fd5ff1ea95
Matthew Philps <philps.matthew@gmail.com>
2020-02-10 10:30:27 +0900

    wip dry gun stats

---
+++ src/data/firearmParts.js
@@ -0,0 +1,157 @@
+//9mm Para
+
+// Carl G, sten, owen
+export const averageSMG9mmParabellumFMJ = {
+  type: 'FMJ',
+  pen: [2.3, 2.1, 1.8, 1.4, 1.1, 0.5, 0.2, 0.1],
+  dc: [3, 3, 3, 2, 1, 1, 1, 1],
+};
+
+// mat 49
+export const aboveAverageSMG9mmParabellumFMJ = {
+  type: 'FMJ',
+  pen: [2.4, 2.2, 1.9, 1.5, 1.1, 0.5, 0.2, 0.1],
+  dc: [3, 3, 3, 2, 2, 1, 1, 1],
+};
+
+// f1
+export const belowAverageSMG9mmParabellumFMJ = {
+  type: 'FMJ',
+  pen: [2.1, 1.9, 1.6, 1.3, 1.0, 0.4, 0.2, 0.1],
+  dc: [3, 3, 2, 2, 1, 1, 1, 1],
+};
+
+// fn mk1, mab pa15
+export const averagePistol9mmParabellumFMJ = belowAverageSMG9mmParabellumFMJ;
+
+//45ACP
+
+// m3a1, tommy
+export const averageSMG45AcpFMJ = {
+  type: 'FMJ',
+  pen: [1.7, 1.5, 1.3, 1.0, 0.8, 0.4, 0.2, 0.1],
+  dc: [3, 3, 2, 1, 1, 1, 1, 1],
+};
+
+// m1911
+export const averagePistol45AcpFMJ = {
+  type: 'FMJ',
+  pen: [1.6, 1.5, 1.2, 1.0, 0.8, 0.3, 0.2, 0.1],
+  dc: [3, 3, 2, 1, 1, 1, 1, 1],
+};
+
+// 7.62mm Tokarev
+
+// ppsh, pps43
+export const averageSMG762TokarevFMJ = {
+  type: 'FMJ',
+  pen: [3.6, 3.3, 2.8, 2.3, 1.8, 0.8, 0.4, 0.2],
+  dc: [4, 4, 3, 2, 2, 1, 1, 1],
+};
+
+export const pps = {
+  type: 'FMJ',
+  pen: [3.6, 3.3, 2.8, 2.3, 1.8, 0.8, 0.4, 0.2],
+  dc: [4, 4, 3, 2, 2, 1, 1, 1],
+};
+
+// tokarev tt33 pistol
+export const averagePistol762TokarevFMJ = {
+  type: 'FMJ',
+  pen: [2.6, 2.4, 2, 1.6, 1.3, 0.6, 0.2, 0.1],
+  dc: [3, 3, 2, 2, 1, 1, 1, 1],
+};
+
+
+//Aims
+
+//SMGs
+
+export const owen = {
+  ac: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+  mod: [-23, -13, -10, -8, -7, -5, -4, -4, -3, -2, -1],
+};
+
+export const tommy = {
+  ac: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+  mod: [-24, -14, -10, -8, -7, -6, -5, -4, -3, -2, -1],
+};
+
+export const f1 = {
+  ac: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+  mod: [-23, -12, -9, -8, -6, -5, -4, -3, -3, -2],
+};
+
+export const mat49 = {
+  ac: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  mod: [-23, -12, -9, -8, -6, -5, -4, -3, -2],
+};
+
+export const ppsh = {
+  ac: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  mod: [-23, -13, -9, -8, -6, -5, -4, -3, -2],
+};
+
+export const pps42 = {
+  ac: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  mod: [-22, -12, -9, -7, -6, -5, -4, -3, -2],
+};
+
+export const carlG = {
+  ac: [1, 2, 3, 4, 5, 6, 7, 8],
+  mod: [-22, -12, -9, -7, -6, -5, -4, -3],
+};
+
+export const sten = {
+  ac: [1, 2, 3, 4, 5, 6, 7, 8],
+  mod: [-22, -12, -9, -7, -6, -5, -4, -3],
+};
+
+export const m3a1 = {
+  ac: [1, 2, 3, 4, 5, 6, 7, 8],
+  mod: [-23, -12, -9, -8, -6, -5, -4, -3],
+};
+
+
+// pistols
+
+// fn mk1, lebel m1892, tokarev tt33,
+export const averagePistol6Aims = {
+  ac: [1, 2, 3, 4, 5, 6],
+  mod: [-17, -11, -10, -9, -8, -7],
+};
+
+// export const fnMk1 = {
+//   ac: [1, 2, 3, 4, 5, 6],
+//   mod: [-17, -11, -10, -9, -8, -7],
+// };
+
+// export const lebelM1892 = {
+//   ac: [1, 2, 3, 4, 5, 6],
+//   mod: [-17, -11, -10, -9, -8, -7],
+// };
+
+// export const tokarevTT33 = {
+//   ac: [1, 2, 3, 4, 5, 6],
+//   mod: [-17, -11, -10, -9, -8, -7],
+// };
+
+export const belowAveragePistol6Aims = {
+  ac: [1, 2, 3, 4, 5, 6],
+  mod: [-18, -11, -10, -9, -8, -7],
+};
+
+// export const m1911 = {
+//   ac: [1, 2, 3, 4, 5, 6],
+//   mod: [-18, -11, -10, -9, -8, -7],
+// };
+
+// export const mabPa15 = {
+//   ac: [1, 2, 3, 4, 5, 6],
+//   mod: [-18, -11, -10, -9, -8, -7],
+// };
+
+export const makarovPM = {
+  ac: [1, 2, 3, 4, 5],
+  mod: [-16, -11, -10, -9, -8],
+};
