4d431f1f0be8b06282e2ca9b95891e3bdff9a6f8
Matthew Philps <philps.matthew@gmail.com>
2020-02-12 11:51:45 +0900

    wip refactor firearms data structure

---
+++ src/data/firearms/magazines.js
@@ -0,0 +1,7 @@
+// SMGs
+
+// F1, Owen Mk1
+export const mag9mmParabellumCap34 = { type: 'Mag', weight: 1.4, cap: 34, qty: 0 };
+
+// M3A1, Thompson M1A1
+export const mag45AcpCap30 = { type: 'Mag', weight: 2.0, cap: 30, qty: 0 };
