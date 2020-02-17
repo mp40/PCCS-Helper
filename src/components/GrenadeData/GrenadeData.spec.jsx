7c204ca9ce24bd63a7428ff178e517c9a42ccb29
Matthew Philps <philps.matthew@gmail.com>
2020-02-17 09:02:29 +0900

    wip grenades refactor

--- src/components/GrenadeData/GrenadeData.spec.jsx
+++ src/components/GrenadeData/GrenadeData.spec.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import GrenadeData, { prepareDataForRender } from './index';
+import GrenadeData from './index';
 
 const getGrenadeData = () => ({
   name: 'L2 A2',
   qty: 1,
-  l: 3.3,
-  w: 0.9,
+  length: 3.3,
+  weight: 0.9,
   at: 3,
   fl: 2,
   r: 16,
   data: {
-    pen: [3.5, 2.4, 2.2, 1.8, 1.5, '1.0', 0.4],
-    dc: [10, 2, 2, 2, 2, 1, 1],
-    bshc: ['*2h', '*3', 77, 19, 8, 2, -1],
-    bc: ['15k', 747, 184, 55, 28, 12, 4],
+    pen: ['PEN', 3.5, 2.4, 2.2, 1.8, 1.5, '1.0', 0.4],
+    dc: ['DC', 10, 2, 2, 2, 2, 1, 1],
+    bshc: ['BSHC', '*2h', '*3', 77, 19, 8, 2, -1],
+    bc: ['BC', '15k', 747, 184, 55, 28, 12, 4],
   },
+  heading: 'standard',
 });
 
 const getWilliePeteData = () => ({
@@ -29,13 +30,14 @@ const getWilliePeteData = () => ({
   smk: 4,
   dur: 3,
   data: {
-    bwphc: ['*3H', '*4', '*4', 24, 10, 5, 3, 2, 1],
-    pdBody: [48, 43, 40, 35, 32, 29, 27, 25, 22],
-    pdLimb: [9, 8, 7, 7, 6, 6, 6, 5, 5],
-    pd0: ['10K', 450, 20, 1],
-    pd4: ['76K', '34H', 150, 8],
-    pd7: ['57T', '25K', '11H', 56],
+    bwphc: ['BWPHC', '*3H', '*4', '*4', 24, 10, 5, 3, 2, 1],
+    pdBody: ['PD Body', 48, 43, 40, 35, 32, 29, 27, 25, 22],
+    pdLimb: ['PD Limb', 9, 8, 7, 7, 6, 6, 6, 5, 5],
+    pd0: ['PDs TS 0', '10K', 450, 20, 1, '', '', '', '', ''],
+    pd4: ['PDs TS 4', '76K', '34H', 150, 8, '', '', '', '', ''],
+    pd7: ['PDs TS 7', '57T', '25K', '11H', 56, '', '', '', '', ''],
   },
+  heading: 'williePete',
 });
 
 const getSmokeData = () => ({
@@ -48,7 +50,8 @@ const getSmokeData = () => ({
   r: 12,
   smk: 3,
   dur: 60,
-  data: null,
+  data: {},
+  heading: 'seeRuleBook',
 });
 
 const getTearGasData = () => ({
@@ -62,7 +65,8 @@ const getTearGasData = () => ({
   sp: 3,
   smk: 3,
   dur: 12,
-  data: null,
+  data: {},
+  heading: 'seeRuleBook',
 });
 
 const getFlashBang = () => ({
@@ -73,7 +77,8 @@ const getFlashBang = () => ({
   at: 3,
   fl: 1,
   r: 20,
-  data: null,
+  data: {},
+  heading: 'seeRuleBook',
 });
 
 
@@ -81,97 +86,47 @@ describe('grenade data table', () => {
   const wrapper = shallow(<GrenadeData grenade={getGrenadeData()} />);
   describe('physical data', () => {
     it('should render length value', () => {
-      expect(wrapper.find('.grenadeDataRowOne').text()).toContain('L3.3');
+      expect(wrapper.find('.grenadeTableBody').childAt(0).text()).toContain('L3.3');
     });
     it('should render weight value', () => {
-      expect(wrapper.find('.grenadeDataRowTwo').text()).toContain('W0.9');
+      expect(wrapper.find('.grenadeTableBody').childAt(1).text()).toContain('W0.9');
     });
     it('should render arm time value', () => {
-      expect(wrapper.find('.grenadeDataRowThree').text()).toContain('AT3');
+      expect(wrapper.find('.grenadeTableBody').childAt(2).text()).toContain('AT3');
     });
     it('should render fuse length value', () => {
-      expect(wrapper.find('.grenadeDataRowFour').text()).toContain('FL2');
+      expect(wrapper.find('.grenadeTableBody').childAt(3).text()).toContain('FL2');
     });
     it('should render range value', () => {
-      expect(wrapper.find('.grenadeDataRowFive').text()).toContain('R16');
+      expect(wrapper.find('.grenadeTableBody').childAt(4).text()).toContain('R16');
     });
   });
   describe('explosive data', () => {
     it('should render PEN data', () => {
-      expect(wrapper.find('.grenadeDataRowOne').text()).toContain('PEN3.52.42.21.81.51.00.4');
+      expect(wrapper.find('.grenadeTableBody').childAt(0).text()).toContain('PEN3.52.42.21.81.51.00.4');
     });
     it('should render DC data', () => {
-      expect(wrapper.find('.grenadeDataRowTwo').text()).toContain('DC10222211');
+      expect(wrapper.find('.grenadeTableBody').childAt(1).text()).toContain('DC10222211');
     });
     it('should render BSHC data', () => {
-      expect(wrapper.find('.grenadeDataRowThree').text()).toContain('BSHC*2h*3771982-1');
+      expect(wrapper.find('.grenadeTableBody').childAt(2).text()).toContain('BSHC*2h*3771982-1');
     });
     it('should render BC data', () => {
-      expect(wrapper.find('.grenadeDataRowFour').text()).toContain('BC15k7471845528124');
+      expect(wrapper.find('.grenadeTableBody').childAt(3).text()).toContain('BC15k7471845528124');
     });
   });
-  describe('helpers', () => {
-    it('should return a 2D array for rendering grenade data', () => {
-      const result = prepareDataForRender(getGrenadeData());
-      expect(result[0]).toEqual(['L', 3.3, 'PEN', [3.5, 2.4, 2.2, 1.8, 1.5, '1.0', 0.4]]);
-      expect(result[1]).toEqual(['W', 0.9, 'DC', [10, 2, 2, 2, 2, 1, 1]]);
-      expect(result[2]).toEqual(['AT', 3, 'BSHC', ['*2h', '*3', 77, 19, 8, 2, -1]]);
-      expect(result[3]).toEqual(['FL', 2, 'BC', ['15k', 747, 184, 55, 28, 12, 4]]);
-      expect(result[4]).toEqual(['R', 16, undefined, ['', '', '', '', '', '', '']]);
-    });
-  });
-  describe('non-standard grenades', () => {
-    // const wrap = shallow(<GrenadeData grenade={getWilliePeteData()} />);
-    it('should render M15 WP grenades', () => {
-      const result = prepareDataForRender(getWilliePeteData());
-      expect(result[0]).toEqual(['L', 6, 'BWPHC', ['*3H', '*4', '*4', 24, 10, 5, 3, 2, 1]]);
-      expect(result[1]).toEqual(['W', 1.9, 'PD Body', [48, 43, 40, 35, 32, 29, 27, 25, 22]]);
-      expect(result[2]).toEqual(['AT', 4, 'PD Limb', [9, 8, 7, 7, 6, 6, 6, 5, 5]]);
-      expect(result[3]).toEqual(['FL', 2, 'PDs TS 0', ['10K', 450, 20, 1]]);
-      expect(result[4]).toEqual(['R', 11, 'PDs TS 4', ['76K', '34H', 150, 8]]);
-      expect(result[5]).toEqual(['Smk', 4, 'PDs TS 7', ['57T', '25K', '11H', 56]]);
-      expect(result[6]).toEqual(['Dur', 3, undefined, ['', '', '', '']]);
-    });
+  describe('non-standard headings', () => {
     it('should render correct range brackets for WP grenades', () => {
       const wrapWP = shallow(<GrenadeData grenade={getWilliePeteData()} />);
       expect(wrapWP.find('thead').text()).toBe('C01234568');
-    });
-    it('should render smoke grenades', () => {
-      const result = prepareDataForRender(getSmokeData());
-      expect(result[0]).toEqual(['L', 5.7, undefined, ['', '', '', '', '', '', '']]);
-      expect(result[1]).toEqual(['W', 1.5, undefined, ['', '', '', '', '', '', '']]);
-      expect(result[2]).toEqual(['AT', 4, undefined, ['', '', '', '', '', '', '']]);
-      expect(result[3]).toEqual(['FL', 1, undefined, ['', '', '', '', '', '', '']]);
-      expect(result[4]).toEqual(['R', 12, undefined, ['', '', '', '', '', '', '']]);
-      expect(result[5]).toEqual(['Smk', 3, undefined, ['', '', '', '', '', '', '']]);
-      expect(result[6]).toEqual(['Dur', 60, undefined, ['', '', '', '', '', '', '']]);
     });
     it('should render see rules message instead of range brackets for smoke', () => {
       const wrapSmoke = shallow(<GrenadeData grenade={getSmokeData()} />);
       expect(wrapSmoke.find('thead').text()).toContain('See Rules');
     });
-    it('should render tear gas grenades', () => {
-      const result = prepareDataForRender(getTearGasData());
-      expect(result[0]).toEqual(['L', 5.7, undefined, ['', '', '', '', '', '', '']]);
-      expect(result[1]).toEqual(['W', 1.2, undefined, ['', '', '', '', '', '', '']]);
-      expect(result[2]).toEqual(['AT', 4, undefined, ['', '', '', '', '', '', '']]);
-      expect(result[3]).toEqual(['FL', 1, undefined, ['', '', '', '', '', '', '']]);
-      expect(result[4]).toEqual(['R', 14, undefined, ['', '', '', '', '', '', '']]);
-      expect(result[5]).toEqual(['SP', 3, undefined, ['', '', '', '', '', '', '']]);
-      expect(result[6]).toEqual(['Smk', 3, undefined, ['', '', '', '', '', '', '']]);
-      expect(result[7]).toEqual(['Dur', 12, undefined, ['', '', '', '', '', '', '']]);
-    });
     it('should render see rules message instead of range brackets for tear gas', () => {
       const wrapGas = shallow(<GrenadeData grenade={getTearGasData()} />);
       expect(wrapGas.find('thead').text()).toContain('See Rules');
-    });
-    it('should render flash bang grenades', () => {
-      const result = prepareDataForRender(getFlashBang());
-      expect(result[0]).toEqual(['L', 5, undefined, ['', '', '', '', '', '', '']]);
-      expect(result[1]).toEqual(['W', 0.6, undefined, ['', '', '', '', '', '', '']]);
-      expect(result[2]).toEqual(['AT', 3, undefined, ['', '', '', '', '', '', '']]);
-      expect(result[3]).toEqual(['FL', 1, undefined, ['', '', '', '', '', '', '']]);
-      expect(result[4]).toEqual(['R', 20, undefined, ['', '', '', '', '', '', '']]);
     });
     it('should render correct hedings for flash bangs', () => {
       const wrapFlash = shallow(<GrenadeData grenade={getFlashBang()} />);
---
+++ src/components/GrenadeData/data.js
@@ -0,0 +1,53 @@
+export const stats = [
+  'length',
+  'weight',
+  'at',
+  'fl',
+  'r',
+  'smk',
+  'dur',
+];
+
+export const effects = [
+  'pen',
+  'dc',
+  'bshc',
+  'bc',
+];
+
+export const effectsSpecial = [
+  'bwphc',
+  'pdBody',
+  'pdLimb',
+  'pd0',
+  'pd4',
+  'pd7',
+];
+
+export const text = {
+  length: 'L',
+  weight: 'W',
+  at: 'AT',
+  fl: 'FL',
+  r: 'R',
+  smk: 'Smk',
+  dur: 'Dur',
+  pen: 'PEN',
+  dc: 'DC',
+  bshc: 'BSHC',
+  bc: 'BC',
+  bwphc: 'BWPHC',
+  pdBody: 'PD Body',
+  pdLimb: 'PD Limb',
+  pd0: 'PDs TS 0',
+  pd4: 'PDs TS 4',
+  pd7: 'PDs TS 7',
+};
+
+export const tableHeadings = {
+  standard: ['', '', '', 'C', '0', '1', '2', '3', '5', '10'],
+  williePete: ['', '', '', 'C', '0', '1', '2', '3', '4', '5', '6', '8'],
+  seeRuleBook: ['', '', '', 'See ', 'Rules', '-', 'Book', '10208', '', ''],
+};
+
+export const keys = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l'];
