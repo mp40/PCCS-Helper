be20e3698812da008233bf948062c42c65c08474
Matthew Philps <philps.matthew@gmail.com>
2020-02-28 09:03:45 +0900

    wip refactor terrible gun stat rendering

---
+++ src/components/WeaponsCardWeaponStatsNew/data.js
@@ -0,0 +1,60 @@
+export const weaponCharacteristics = [
+  {
+    data: 'length',
+    abbreviation: 'L',
+  },
+  {
+    data: 'weight',
+    abbreviation: 'W',
+  },
+  {
+    data: '',
+    abbreviation: '',
+  },
+  {
+    data: 'rt',
+    abbreviation: 'RT',
+  },
+  {
+    data: 'rof',
+    abbreviation: 'ROF',
+  },
+  {
+    data: '',
+    abbreviation: '',
+  },
+  {
+    data: ['mag', 'cap'],
+    abbreviation: 'Cap',
+  },
+  {
+    data: ['mag', 'weight'],
+    abbreviation: 'AW',
+  },
+  {
+    data: ['mag', 'type'],
+    abbreviation: 'Mag',
+  },
+  {
+    data: 'kd',
+    abbreviation: 'KD',
+  },
+  {
+    data: 'sab',
+    abbreviation: 'SAB',
+  },
+];
+
+export const defaultTemplate = [
+  'pen',
+  'dc',
+  ['', '', '', '', '', '', '', ''],
+  'pen',
+  'dc',
+  ['', '', '', '', '', '', '', ''],
+  'pen',
+  'dc',
+  'ma',
+  'ba',
+  'tof',
+];
