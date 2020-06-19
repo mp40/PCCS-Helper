385f9804435e2b86986ec8b51241efee8b9fe482
Matthew Philps <philps.matthew@gmail.com>
2020-06-19 11:58:53 +0900

    test

--- src/components/BetaTemp/index.jsx
+++ src/components/BetaTemp/index.jsx
@@ -1,16 +1,14 @@
-import React from 'react';
+import React from "react";
 
 const BetaTemp = () => (
-  <div style={{ marginTop: '10rem' }}>
-    <div style={{ fontSize: '8rem', fontWeight: 'bold', opacity: '0.5' }}>
+  <div style={{ marginTop: "10rem" }}>
+    <div style={{ fontSize: "8rem", fontWeight: "bold", opacity: "0.5" }}>
       BETA VERSION
     </div>
-    <div style={{ fontSize: '4rem', opacity: '0.5' }}>
-      In Development
-    </div>
+    <div style={{ fontSize: "4rem", opacity: "0.5" }}>In Development</div>
   </div>
 );
 
 export default BetaTemp;
 
-// mptodo delete this component once at MVP
+// mptodo delete this component once at MVP..
