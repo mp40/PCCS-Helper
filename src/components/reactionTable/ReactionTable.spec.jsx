cf5ab87153fafb1e4c7b0c2438a4e301a057f8b6
Matthew Philps <philps.matthew@gmail.com>
2021-04-04 09:26:19 +0200

    Refactor reaction table

--- src/components/reactionTable/ReactionTable.spec.jsx
+++ src/components/reactionTable/ReactionTable.spec.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import ReactionTable from './ReactionTable';
+import ReactionTable from '.';
 
 describe('The Reaction Table', () => {
   const sal = 10;
+
   const wrapper = shallow(<ReactionTable sal={sal} />);
+
   it('should render any action roll value', () => {
     expect(wrapper.text()).toContain('Any Action≤ 7');
   });
+
   it('should render any duck roll value', () => {
     expect(wrapper.text()).toContain('Duck/Go Prone≤ 10');
   });
+
   it('should render any reroll value', () => {
     expect(wrapper.text()).toContain('-1 AC & Reroll≤ 11');
   });
