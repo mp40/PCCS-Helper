3d95dc9f8466887535ea0016c6c4eda765ca8fd2
Matthew Philps <philps.matthew@gmail.com>
2021-04-23 08:47:29 +0200

    shooting situation select modal added

---
+++ src/components/LoadedCharacter/Shooting/SituationSelectModal/index.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import CheckBox from '../../../widgets/buttons/CheckBox';
+
+import { getModifierList } from './data';
+
+import styles from './styles.module.css';
+
+const SituationSelectModal = ({ list, bipod, foldingStock, setModal, weaponBasedALM, setWeaponBasedALM }) => {
+  const handleClick = (key) => {
+    const updatedObject = { ...weaponBasedALM };
+    updatedObject[key] = !updatedObject[key];
+
+    setWeaponBasedALM(updatedObject);
+  };
+
+  return (
+    <>
+      <div className={styles.modal} />
+      <div className={`--card ${styles.card}`}>
+        {getModifierList(list, bipod, foldingStock).map((situation) => (
+          <div key={situation.key} className={styles.checkbox}>
+            <span>{situation.text}</span>
+            <span>
+              <CheckBox onClick={() => handleClick(situation.key)} isActive={weaponBasedALM[situation.key]} />
+            </span>
+          </div>
+        ))}
+        <button type="button" className={styles.done} onClick={() => setModal(false)}>Done</button>
+      </div>
+    </>
+  );
+};
+
+SituationSelectModal.propTypes = {
+  list: PropTypes.string.isRequired,
+  foldingStock: PropTypes.bool.isRequired,
+  bipod: PropTypes.bool.isRequired,
+  setModal: PropTypes.func.isRequired,
+  weaponBasedALM: PropTypes.objectOf(PropTypes.bool).isRequired,
+  setWeaponBasedALM: PropTypes.func.isRequired,
+};
+
+export default SituationSelectModal;
