7d9ca64442fe562e7326d72cc71b4cbd9065bfd7
Matthew Philps <philps.matthew@gmail.com>
2021-04-23 08:57:48 +0200

    added stance select modal

---
+++ src/components/LoadedCharacter/Shooting/StanceSelectModal/index.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styles from './styles.module.css';
+
+const StanceSelectModal = ({ setStance, setModal }) => {
+  const stances = ['Standing', 'Kneeling', 'Prone'];
+
+  const handleClick = (stance) => {
+    setStance(stance);
+    setModal(false);
+  };
+
+  return (
+    <>
+      <div className={styles.modal} />
+      <div className={`--card ${styles.card}`}>
+        {stances.map((stance) => (
+          <button type="button" key={stance} onClick={() => handleClick(stance)}>
+            {stance}
+          </button>
+        ))}
+      </div>
+    </>
+  );
+};
+
+StanceSelectModal.propTypes = {
+  setStance: PropTypes.func.isRequired,
+  setModal: PropTypes.func.isRequired,
+};
+
+export default StanceSelectModal;
