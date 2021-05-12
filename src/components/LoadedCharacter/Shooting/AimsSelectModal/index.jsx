ba18ce3b5de6a42dbb755958ffae2f070fa40ab5
Matthew Philps <philps.matthew@gmail.com>
2021-05-12 07:09:25 +0200

    Add select aims modal

---
+++ src/components/LoadedCharacter/Shooting/AimsSelectModal/index.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styles from './styles.module.css';
+
+const AimsSelectModal = ({ aims, maxAims, setAims, setModal }) => {
+  const handleClick = (aims) => {
+    setAims(aims);
+    setModal(false);
+  };
+
+  return (
+    <>
+      <div className="--modal" />
+      <div className={`--card ${styles.card}`}>
+        {[...Array(maxAims).keys()].map((key) => (
+          <button className={aims === key + 1 ? styles.selected : ''} type="button" key={key} onClick={() => handleClick(key + 1)}>
+            {key + 1}
+          </button>
+        ))}
+      </div>
+    </>
+  );
+};
+
+AimsSelectModal.propTypes = {
+  aims: PropTypes.number.isRequired,
+  maxAims: PropTypes.number.isRequired,
+  setAims: PropTypes.func.isRequired,
+  setModal: PropTypes.func.isRequired,
+};
+
+export default AimsSelectModal;
