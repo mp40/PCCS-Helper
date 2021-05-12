d87ed2219f5945fe9836def8e8cd03ca85bd9c7a
Matthew Philps <philps.matthew@gmail.com>
2021-05-12 07:15:07 +0200

    Add shooting firearm for loaded character

---
+++ src/components/LoadedCharacter/Shooting/data.js
@@ -0,0 +1,465 @@
+import { getRangeBrackets } from '../../../data/firearms/rangeBrackets';
+
+export const rangeMods = {
+  1: 33,
+  2: 28,
+  3: 25,
+  4: 23,
+  5: 22,
+  6: 20,
+  7: 19,
+  8: 18,
+  9: 17,
+  11: 16,
+  12: 15,
+  14: 14,
+  16: 13,
+  19: 12,
+  22: 11,
+  25: 10,
+  30: 9,
+  35: 8,
+  40: 7,
+  45: 6,
+  50: 5,
+  65: 3,
+  75: 2,
+  85: 1,
+  100: 0,
+  115: -1,
+  130: -2,
+  150: -3,
+  170: -4,
+  200: -5,
+  230: -6,
+  250: -7,
+  300: -8,
+  350: -9,
+  400: -10,
+};
+
+const oddsOfHitting = [
+  {
+    eal: 28,
+    Single: 99,
+    Auto: 99,
+  },
+  {
+    eal: 27,
+    Single: 98,
+    Auto: 98,
+  },
+  {
+    eal: 26,
+    Single: 96,
+    Auto: 98,
+  },
+  {
+    eal: 25,
+    Single: 94,
+    Auto: 97,
+  },
+  {
+    eal: 24,
+    Single: 90,
+    Auto: 95,
+  },
+  {
+    eal: 23,
+    Single: 86,
+    Auto: 92,
+  },
+  {
+    eal: 22,
+    Single: 80,
+    Auto: 90,
+  },
+  {
+    eal: 21,
+    Single: 74,
+    Auto: 86,
+  },
+  {
+    eal: 20,
+    Single: 67,
+    Auto: 82,
+  },
+  {
+    eal: 19,
+    Single: 60,
+    Auto: 77,
+  },
+  {
+    eal: 18,
+    Single: 53,
+    Auto: 73,
+  },
+  {
+    eal: 17,
+    Single: 46,
+    Auto: 68,
+  },
+  {
+    eal: 16,
+    Single: 39,
+    Auto: 62,
+  },
+  {
+    eal: 15,
+    Single: 33,
+    Auto: 57,
+  },
+  {
+    eal: 14,
+    Single: 27,
+    Auto: 52,
+  },
+  {
+    eal: 13,
+    Single: 22,
+    Auto: 47,
+  },
+  {
+    eal: 12,
+    Single: 18,
+    Auto: 43,
+  },
+  {
+    eal: 11,
+    Single: 15,
+    Auto: 38,
+  },
+  {
+    eal: 10,
+    Single: 12,
+    Auto: 34,
+  },
+  {
+    eal: 9,
+    Single: 9,
+    Auto: 31,
+  },
+  {
+    eal: 8,
+    Single: 7,
+    Auto: 27,
+  },
+  {
+    eal: 7,
+    Single: 6,
+    Auto: 24,
+  },
+  {
+    eal: 6,
+    Single: 5,
+    Auto: 21,
+  },
+  {
+    eal: 5,
+    Single: 4,
+    Auto: 19,
+  },
+  {
+    eal: 4,
+    Single: 3,
+    Auto: 17,
+  },
+  {
+    eal: 3,
+    Single: 2,
+    Auto: 15,
+  },
+  {
+    eal: 2,
+    Single: 2,
+    Auto: 13,
+  },
+  {
+    eal: 1,
+    Single: 1,
+    Auto: 11,
+  },
+  {
+    eal: 0,
+    Single: 1,
+    Auto: 10,
+  },
+  {
+    eal: -1,
+    Single: 1,
+    Auto: 9,
+  },
+  {
+    eal: -2,
+    Single: 0,
+    Auto: 8,
+  },
+  {
+    eal: -3,
+    Single: false,
+    Auto: 7,
+  },
+  {
+    eal: -4,
+    Single: false,
+    Auto: 6,
+  },
+  {
+    eal: -5,
+    Single: false,
+    Auto: 6,
+  },
+  {
+    eal: -6,
+    Single: false,
+    Auto: 4,
+  },
+  {
+    eal: -8,
+    Single: false,
+    Auto: 3,
+  },
+  {
+    eal: -10,
+    Single: false,
+    Auto: 2,
+  },
+  {
+    eal: -15,
+    Single: false,
+    Auto: 1,
+  },
+  {
+    eal: -17,
+    Single: false,
+    Auto: 0,
+  },
+  {
+    eal: -22,
+    Single: false,
+    Auto: false,
+  },
+];
+
+export const shooterStance = {
+  Standing: 0,
+  Kneeling: 3,
+  Prone: 6,
+};
+
+export const defaultSituationState = {
+  braced: false,
+  slingSupport: false,
+  hipFire: false,
+  rifleOneHand: false,
+  smgOneHand: false,
+  pistolOneHand: false,
+  foldingStockNotUsed: false,
+  pistolDoubleAction: false,
+  bipodNotBraced: false,
+  bipodBraced: false,
+  tripodMount: false,
+  pintleMount: false,
+  turretMount: false,
+};
+
+export const defaultVisibilityState = {
+  lighting: 'Good',
+  muzzleFlash: false,
+  smokeFogHaze: false,
+  lookingIntoLight: false,
+  opticalUnder8: false,
+  opticsBroken: false,
+  aasBroken: false,
+  sightsBroken: false,
+  teargasNoMask: false,
+  notLooking: false,
+};
+
+const movementMods = {
+  // Rg: [.5, 1, 2, 3, 4, 10] <- speed in array, * = no max aim
+  ranges: [10, 20, 40, 70, 100, 200, 300],
+  speeds: [0.5, 1, 2, 3, 4, 10],
+  10: [-6, -8, -10, -10, -10, -10],
+  20: [-5, -6, -8, -10, -10, -10],
+  40: [-5, -5, -6, -7, -8, -10],
+  70: [-5, -5, -5, -6, -6, -10],
+  100: [-5, -5, -5, -5, -6, -8],
+  200: [-5, -5, -5, -5, -5, -6],
+  300: [-5, -5, -5, -5, -5, -5],
+  noMax70: [true, false, false, false, false, false],
+  noMax100: [true, true, false, false, false, false],
+  noMax200: [true, true, true, false, false, false],
+  noMax300: [true, true, true, true, false, false],
+};
+
+export const targetSizeMods = {
+  'Look Over/Around': { size: -4, elev: -3, width: -3 },
+  'Fire Over/Around': { size: 0, elev: 2, width: 2 },
+  'Standing Exposed': { size: 7, elev: 14, width: 1 },
+  'Kneeling Exposed': { size: 6, elev: 11, width: 3 },
+  'Prone/Crawl': { size: 2, elev: 2, width: 2 },
+  Running: { size: 8, elev: 14, width: 1 },
+  'Low Crouch': { size: 7, elev: 11, width: 2 },
+  'Hands and Knees': { size: 6, elev: 8, width: 1 },
+  'Low Prone': { size: 1, elev: 0, width: 5 },
+};
+
+export const findSpeedMods = (combinedSpeed, range) => {
+  if (combinedSpeed === 0) {
+    return { mod: 0, noMax: true };
+  }
+
+  let rangeKey;
+  let speedIndex;
+
+  for (let i = 0; i < movementMods.ranges.length; i += 1) {
+    rangeKey = movementMods.ranges[i];
+
+    if (movementMods.ranges[i] > range) {
+      break;
+    }
+  }
+
+  for (let i = 0; i < movementMods.speeds.length; i += 1) {
+    speedIndex = i;
+
+    if (movementMods.speeds[i] > combinedSpeed) {
+      break;
+    }
+  }
+
+  const noMax = movementMods[`noMax${rangeKey}`] ? movementMods[`noMax${rangeKey}`][speedIndex] : false;
+
+  const result = {
+    mod: movementMods[rangeKey][speedIndex],
+    noMax,
+  };
+
+  return result;
+};
+
+const lightingALM = {
+  Good: 0,
+  Dusk: -2,
+  'Full Moon': -4,
+  'Half Moon': -6,
+  'No Moon': -12,
+};
+
+const otherVisibilityALM = {
+  muzzleFlash: -10,
+  smokeFogHaze: -6,
+  lookingIntoLight: -8,
+  sightsBroken: -4,
+  teargasNoMask: -8,
+  notLooking: -14,
+};
+
+export const getVisibilityALM = (state) => {
+  const lighting = lightingALM[state.lighting];
+
+  let otherPenalites = 0;
+
+  Object.keys(state).forEach((key) => {
+    if (state[key] === true) {
+      otherPenalites += otherVisibilityALM[key];
+    }
+  });
+
+  return lighting + otherPenalites;
+};
+
+const brancedModifiers = {
+  Standing: 4,
+  Kneeling: 2,
+  Prone: 1,
+};
+
+const situationModifiers = {
+  hipFire: -6,
+  rifleOneHand: -7,
+  smgOneHand: -6,
+  pistolOneHand: -4,
+  foldingStockNotUsed: -4,
+  pistolDoubleAction: -3,
+  bipodNotBraced: -2,
+  bipodBraced: 3,
+  tripodMount: 5,
+  pintleMount: 7,
+  turretMount: 11,
+};
+
+export const getSituationALM = (state, stance, aimTime) => {
+  let result = 0;
+
+  if (state.braced) {
+    result += brancedModifiers[stance];
+  }
+
+  if (aimTime > 7 && state.slingSupport) {
+    result += 1;
+  }
+
+  Object.keys(situationModifiers).forEach((situation) => {
+    if (state[situation]) {
+      result += situationModifiers[situation];
+    }
+  });
+
+  return result;
+};
+
+export const getAimTimeMod = (aimData, aims) => {
+  let result = 1;
+
+  for (let i = 0; i < aimData.ac.length; i += 1) {
+    if (aimData.ac[i] <= aims) {
+      result = aimData.mod[i];
+    }
+  }
+
+  return result;
+};
+
+export const getOddsOfHitting = (alm, sizeMod, rof) => {
+  const eal = alm + sizeMod;
+  const rofType = rof === '3RB' ? 'Auto' : rof;
+
+  if (eal >= 28) {
+    return 99;
+  }
+
+  if (eal <= -22) {
+    return 'NA';
+  }
+
+  let odds = false;
+
+  for (let i = 1; i < oddsOfHitting.length; i += 1) {
+    if (oddsOfHitting[i - 1].eal > eal || oddsOfHitting[i].eal === eal) {
+      odds = oddsOfHitting[i][rofType];
+    }
+
+    if (oddsOfHitting[i].eal < eal) {
+      break;
+    }
+  }
+
+  return odds === false ? 'NA' : odds;
+};
+
+export const getWeaponRangeIndex = (list, range) => {
+  const rangeBrackets = getRangeBrackets(list);
+
+  let index;
+
+  for (let i = 0; i < rangeBrackets.length; i += 1) {
+    if (rangeBrackets[i] >= range) {
+      index = i;
+      break;
+    }
+  }
+
+  return index;
+};
---
+++ src/components/LoadedCharacter/Shooting/index.jsx
@@ -0,0 +1,261 @@
+import React, { useState, useEffect, Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+import RangeSelectModal from './RangeSelectModal';
+import StanceSelectModal from './StanceSelectModal';
+import TargetSizeSelectModal from './TargetSizeModal';
+import MovementSelectModal from './MovementSelectModal';
+import VisibilitySelectModal from './VisibilitySelectModal';
+import SituationSelectModal from './SituationSelectModal';
+import AimsSelectModal from './AimsSelectModal';
+import FireSelector from './FireSelector';
+import Aiming from './Aiming';
+
+import { gunObjShape } from '../../../helpers/proptypeShapes';
+
+import { getRecoilRecoveryValue } from '../../../data/advancedRules/recoilRecovery';
+
+import {
+  getAimTimeMod,
+  rangeMods,
+  getVisibilityALM,
+  getSituationALM,
+  findSpeedMods,
+  shooterStance,
+  targetSizeMods,
+  getOddsOfHitting,
+  defaultSituationState,
+  defaultVisibilityState,
+  getWeaponRangeIndex,
+} from './data';
+
+import styles from './styles.module.css';
+
+const LoadedCharacterShooting = ({
+  firearm,
+  sal,
+  level,
+  setFirearm,
+}) => {
+  const [rof, setRof] = useState(!firearm.selector ? 'Single' : 'Auto');
+  const [roundsFired, setRoundsFired] = useState(0);
+  const [sab, setSab] = useState(0);
+  const [ammoType, setAmmoType] = useState(0);
+  const [aims, setAims] = useState(1);
+  const [stance, setStance] = useState('Standing');
+  const [range, setRange] = useState(100);
+  const [size, setSize] = useState('Standing Exposed');
+  const [movement, setMovement] = useState({ shooter: 0, target: 0 });
+  const [visibility, setVisibility] = useState(defaultVisibilityState);
+  const [weaponBasedALM, setWeaponBasedALM] = useState(defaultSituationState);
+  const [modal, setModal] = useState(false);
+
+  useEffect(() => {
+    setAims(1);
+    setRoundsFired(0);
+    setSab(0);
+    setWeaponBasedALM(defaultSituationState);
+    setAmmoType(0);
+  }, [firearm]);
+
+  const movementModInfo = findSpeedMods(movement.shooter + movement.target, range);
+
+  const calculateALM = () => {
+    let result = sal - sab;
+
+    result += getAimTimeMod(firearm.aim, aims);
+    result += rangeMods[range];
+    result += shooterStance[stance];
+    result += movementModInfo.mod;
+    result += getVisibilityALM(visibility);
+    result += getSituationALM(weaponBasedALM, stance, aims);
+
+    return result;
+  };
+
+  const getTargetSizeMod = () => {
+    if (rof === 'Single') {
+      return targetSizeMods[size].size;
+    }
+
+    return targetSizeMods[size].elev;
+  };
+
+  const alm = calculateALM();
+  const targetSizeMod = getTargetSizeMod();
+  const autoROF = Number(String(firearm.rof).split('*').pop());
+  const maxAims = firearm.aim.ac.slice(-1)[0];
+
+  const getRoundsPerShot = () => {
+    if (rof === 'Auto') {
+      return autoROF;
+    }
+
+    if (rof === '3RB') {
+      return 3;
+    }
+
+    return 1;
+  };
+
+  const getRoundsClassName = () => {
+    const { cap } = firearm.mag[0];
+
+    if (roundsFired >= cap) {
+      return styles.emptyMag;
+    }
+
+    if (cap - roundsFired <= getRoundsPerShot()) {
+      return styles.lowMag;
+    }
+
+    return '';
+  };
+
+  const almButtons = [
+    {
+      text: 'Range',
+      key: 'range',
+      value: range,
+    },
+    {
+      text: 'Shooter Stance',
+      key: 'stance',
+      value: stance,
+    },
+    {
+      text: 'Target Size',
+      key: 'target',
+      value: size,
+    },
+    {
+      text: 'Movement',
+      key: 'movement',
+      value: `Shooter:${movement.shooter} | Target:${movement.target}`,
+    },
+    {
+      text: 'Situation',
+      key: 'situation',
+      value: `ALM: ${getSituationALM(weaponBasedALM, stance, aims)}`,
+    },
+    {
+      text: 'Visibility',
+      key: 'visibility',
+      value: `ALM: ${getVisibilityALM(visibility)}`,
+    },
+  ];
+
+  const handleNewShot = () => {
+    setRoundsFired(roundsFired + getRoundsPerShot());
+    setAims(1);
+  };
+
+  const handleSustainedFire = () => {
+    const roundsExpended = autoROF;
+
+    setRoundsFired(roundsFired + roundsExpended);
+    setSab(sab + firearm.sab);
+  };
+
+  const handleCeaseFire = () => {
+    setSab(0);
+    setAims(1);
+  };
+
+  const handleReload = () => {
+    setSab(0);
+    setAims(1);
+    setRoundsFired(0);
+  };
+
+  return (
+    <div className={`${styles.wrapper} --card`}>
+      <div className={styles.header}>
+        <span>{firearm.name}</span>
+        <button
+          aria-label="close"
+          className={styles.close}
+          type="button"
+          onClick={() => setFirearm(false)}
+        />
+      </div>
+
+      <div className={styles.ammoTypes}>
+        {firearm.projectiles.map((projectile, index) => (
+          <Fragment key={projectile.type}>
+            <button type="button" key={projectile.type} onClick={() => setAmmoType(index)}>
+              {projectile.type}
+            </button>
+            <div className={styles.ammoName}>{projectile.type}</div>
+            <div className={`${styles.ammoMarker} ${index === ammoType ? styles.selected : ''}`} />
+          </Fragment>
+        ))}
+      </div>
+
+      <div className={styles.data}>
+        <div>
+          <span>{`ALM: ${alm}`}</span>
+          <span>{`EAL: ${alm + targetSizeMod}`}</span>
+        </div>
+        <div>
+          <span>{`PEN: ${firearm.projectiles[ammoType].pen[getWeaponRangeIndex(firearm.list, range)]}`}</span>
+          <span>{`DC: ${firearm.projectiles[ammoType].dc[getWeaponRangeIndex(firearm.list, range)]}`}</span>
+          {rof !== 'Single'
+            && <span>{`MA: ${firearm.ma[getWeaponRangeIndex(firearm.list, range)]}`}</span>}
+        </div>
+        <span>{`Recoil Recovery: ${getRecoilRecoveryValue(firearm.kd, level)}`}</span>
+      </div>
+
+      <div className={styles.modalButtonWrapper}>
+        {almButtons.map((button) => (
+          <button key={button.key} type="button" onClick={() => setModal(button.key)}>
+            <span>{button.text}</span>
+            <span>{button.value}</span>
+          </button>
+        ))}
+
+      </div>
+
+      <Aiming aims={aims} maxAims={maxAims} setAims={setAims} setModal={setModal} />
+
+      <div className={styles.firing}>
+        <div>
+          <span>{'Rounds Fired: '}</span>
+          <span className={getRoundsClassName()}>{roundsFired}</span>
+          <span>{` / ${firearm.mag[0].cap}`}</span>
+        </div>
+        <div>{`Hit Chance: ${getOddsOfHitting(alm, targetSizeMod, rof)}%`}</div>
+
+        <div className={styles.pewpew}>
+          <FireSelector single={!firearm.selector} selector={String(firearm.rof)} rof={rof} setRof={setRof} />
+          <div>
+            {sab === 0 && <button type="button" onClick={() => handleNewShot()}>FIRE</button>}
+            {sab > 0 && <button type="button" onClick={() => handleCeaseFire()}>Cease Fire</button>}
+            {rof === 'Auto' && <button type="button" onClick={() => handleSustainedFire()}>Sustained Fire</button>}
+            <button type="button" onClick={() => handleReload()}>Reload</button>
+          </div>
+
+        </div>
+
+      </div>
+
+      {modal === 'range' && <RangeSelectModal setRange={setRange} setModal={setModal} />}
+      {modal === 'stance' && <StanceSelectModal setStance={setStance} setModal={setModal} />}
+      {modal === 'target' && <TargetSizeSelectModal setSize={setSize} setModal={setModal} />}
+      {modal === 'movement' && <MovementSelectModal setMovement={setMovement} setModal={setModal} movement={movement} />}
+      {modal === 'situation' && <SituationSelectModal list="rifles" bipod foldingStock={String(firearm.length).includes('/')} setModal={setModal} weaponBasedALM={weaponBasedALM} setWeaponBasedALM={setWeaponBasedALM} />}
+      {modal === 'visibility' && <VisibilitySelectModal setModal={setModal} visibility={visibility} setVisibility={setVisibility} />}
+      {modal === 'aims' && <AimsSelectModal aims={aims} maxAims={maxAims} setAims={setAims} setModal={setModal} />}
+    </div>
+
+  );
+};
+
+LoadedCharacterShooting.propTypes = {
+  firearm: gunObjShape.isRequired,
+  sal: PropTypes.number.isRequired,
+  level: PropTypes.number.isRequired,
+  setFirearm: PropTypes.func.isRequired,
+};
+
+export default LoadedCharacterShooting;
