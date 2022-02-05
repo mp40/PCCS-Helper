c3520d1d716456d09d3ec9fd9627db13c9c67cb7
mp40 <philps.matthew@gmail.com>
2022-02-05 15:50:14 +0100

    Add cypress

---
+++ cypress/integration/header/index.spec.js
@@ -0,0 +1,16 @@
+/// <reference types="cypress" />
+
+describe('Header', () => {
+    before(() => {
+      cy.visit('/') 
+    });
+
+
+    it.skip('should return to home view when home button clicked', () => {
+        cy.get('button').contains('Create Character').click()
+
+        cy.get('.menuBar').within(() => {
+            cy.get('.home').click();
+        })
+    })
+})
